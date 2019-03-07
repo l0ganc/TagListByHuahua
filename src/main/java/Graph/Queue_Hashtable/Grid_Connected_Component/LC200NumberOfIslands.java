851a5846bfcda008076cdb129d09d47c4ec75133
l0ganc <jingyige@usc.edu>
2019-03-06 19:50:10 -0800

    LC200 超经典

---
+++ src/main/java/Graph/Queue_Hashtable/Grid_Connected_Component/LC200NumberOfIslands.java
@@ -0,0 +1,36 @@
+package Graph.Queue_Hashtable.Grid_Connected_Component;
+
+/**
+ * time : O(m * n)
+ * space : O(n)
+ */
+public class LC200NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
